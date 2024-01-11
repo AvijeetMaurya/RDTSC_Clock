3b211cae63e2a4bb8f8c5f5627d34dfd0a114204
Avijeet Maurya <avijeet@zanskar.xyz>
2024-01-11 12:54:42 +0530

    pre-calculate boot timestamp

--- rdtsc_clock.cpp
+++ rdtsc_clock.cpp
@@ -15,24 +15,22 @@ namespace RDTSC_Clock {
 
     double internal::RDTSC_TICK_FREQ;
     unsigned int temp;
-    internal::InitialState initialState;
+    unsigned long long internal::timestamp;
     std::jthread worker;
 
-    void sync(std::stop_token status, internal::InitialState& initialState) {
+    void sync(std::stop_token status, unsigned long long& initialTimestamp) {
         while (!status.stop_requested()) {
             std::this_thread::sleep_for(std::chrono::seconds(10));
-            initialState.timestamp = std::chrono::system_clock::now().time_since_epoch().count();
-            initialState.cycles = __rdtscp(&temp);
+            initialTimestamp = std::chrono::system_clock::now().time_since_epoch().count() - (__rdtscp(&temp) / internal::RDTSC_TICK_FREQ);
         }
     }
 
     void init() {
         internal::RDTSC_TICK_FREQ = GET_RDTSC_TICK_FREQ();
         //std::cout << "RDTSC_TICK_FREQ: " << internal::RDTSC_TICK_FREQ << '\n';
-        initialState.timestamp  = std::chrono::system_clock::now().time_since_epoch().count();
-        initialState.cycles = __rdtscp(&temp);
+        internal::timestamp = std::chrono::system_clock::now().time_since_epoch().count() - (__rdtscp(&temp) / internal::RDTSC_TICK_FREQ);
 
-        worker = std::jthread(sync, std::ref(initialState));
+        worker = std::jthread(sync, std::ref(internal::timestamp));
         /* Pin to specific CPU
         cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
@@ -42,8 +40,7 @@ namespace RDTSC_Clock {
     }
 
     unsigned long long now(unsigned long long elapsedCycles) {
-        auto timestamp = (initialState.timestamp + static_cast<unsigned long long>(static_cast<long double>(elapsedCycles - initialState.cycles) / internal::RDTSC_TICK_FREQ));
-        return timestamp;
+        return internal::timestamp + static_cast<unsigned long long>(elapsedCycles / internal::RDTSC_TICK_FREQ);
     }
 
     void exit() {
--- rdtsc_clock.hpp
+++ rdtsc_clock.hpp
@@ -6,12 +6,7 @@
 namespace RDTSC_Clock {
     namespace internal {
         extern double RDTSC_TICK_FREQ;
-        struct InitialState {
-        unsigned long long timestamp;
-        unsigned long long cycles;
-        };
-        static_assert(sizeof(InitialState) <= 16);
-        extern InitialState initialState;
+        extern unsigned long long timestamp;
     }
     void init();
     unsigned long long now(unsigned long long elapsedCycles);
